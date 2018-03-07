276505573705b0ff3d1a688b47405b88ee506b45
QuantumTM <samuelwoodhams@gmail.com>
2018-03-07 00:37:11 +0000

    fixed install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!bin/bash
-CONFIG="dotbot.conf.yaml"
+CONFIG="dotfiles.conf.yaml"
 DOTBOT_DIR="dotbot"
 DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${BASEDIR}"
+git submodule update --init --recursive "${DOTBOT_DIR}"
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
